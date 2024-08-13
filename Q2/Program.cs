4ad9e9ff548655d6fc8461ee1b10a70d57a849a3
João Lucas <joaolucas17.ce@gmail.com>
2024-08-13 11:55:17 -0300

    Criando questão 2

---
+++ Q2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double IMC, peso, altura;
+        Console.WriteLine("Informe o seu peso :");
+        double.TryParse(Console.ReadLine(), out peso);
+         Console.WriteLine("Informe a sua altura :");
+        double.TryParse(Console.ReadLine(), out altura);
+        IMC = peso/Math.Pow(altura,2);
+        Console.WriteLine($"O seu imc é igual a: {IMC}");
+        Console.ReadLine(); 
+    }
+}
