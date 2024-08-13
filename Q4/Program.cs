85a7a7ef5ce354c3f24aca1214bfe72645ca2e8b
João Lucas <joaolucas17.ce@gmail.com>
2024-08-13 12:26:43 -0300

    Atualizando questão 4

---
+++ Q4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double feh, conver;
+        Console.WriteLine("Informe a temperatura em  fahrenheit:");
+        double.TryParse(Console.ReadLine(), out feh);
+        conver =  ((feh - 32) * 5/9);
+        Console.WriteLine($"A conversão para celsius {conver}");
+        Console.ReadLine(); 
+    }
+}
