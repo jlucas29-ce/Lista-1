45b3c0e01639729703d6ceb3b96226b278df427b
João Lucas <joaolucas17.ce@gmail.com>
2024-08-13 12:09:43 -0300

    Salvando questão 3

--- Q3/Program.cs
+++ Q3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double salario, rea;
+        Console.WriteLine("Informe o seu salario:");
+        double.TryParse(Console.ReadLine(), out salario);
+        rea = salario * 1.2;
+        Console.WriteLine($"O seu salario com reajuste: {rea}");
+        Console.ReadLine(); 
+    }
+}
+
